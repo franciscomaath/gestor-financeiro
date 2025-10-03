f1de2fd83ec6b4147973faed5fb90c6f1487d760
glubao <40120350+glubao@users.noreply.github.com>
2025-10-03 11:01:52 -0300

    feat: Initial App.jsx

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="app-header">
+            <h1>Pynance App</h1>
+            <p>Seu gestor financeiro pessoal.</p>
+        </header>
+    )
+}
+
+export default Header;
